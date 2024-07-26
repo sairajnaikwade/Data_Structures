8997c99fc578bd36520a0dd155844f4bff13b722
Sairaj Naikwade <141031345+sairajnaikwade@users.noreply.github.com>
2024-07-26 21:17:38 +0530

    Update LinearSearch.c

--- #C/Searching Algorithms/LinearSearch.c
+++ #C/Searching Algorithms/LinearSearch.c
@@ -1,1 +1,40 @@
+//Linear Search
+
+#include<stdio.h>
+void main()
+ {
+    int a[20],n,key,i,ch;
+    do{
+    int flag=0;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    printf("\nEnter the elements of array:");
+    for(i=0;i<n;i++)
+    {
+      printf("\na[%d] = ",i);
+      scanf("%d",&a[i]);
+    }
+    printf("\nEnter element for search:");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+    {
+       if(key==a[i])
+       {
+         flag=1;
+         break;
+       }
+    }
+    if(flag==1)
+    {
+      printf("\na[%d] = %d",i,a[i]);
+    }else
+    {
+      printf("\nInvalid Number");
+    }
+    printf("\nDo you want to continue ? Press 1 for yes:");
+    scanf("%d",&ch);
+    }while(ch==1);
+}
+
+
 
