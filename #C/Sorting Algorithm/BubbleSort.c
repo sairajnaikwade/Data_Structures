927c7b517259f8686d7558551362b98d3bf8c341
Sairaj Naikwade <141031345+sairajnaikwade@users.noreply.github.com>
2024-07-26 21:07:22 +0530

    Update BubbleSort.c

--- #C/Sorting Algorithm/BubbleSort.c
+++ #C/Sorting Algorithm/BubbleSort.c
@@ -1,1 +1,33 @@
+//bubble sort:-arranging numbers in ascending order
+
+#include<stdio.h>
+void main()
+{
+    int a[10],s,i,j,temp;
+    printf("Enter the size of an array:");
+    scanf("%d",&s);
+    printf("\nEnter the elements of array:");
+    for(i=0;i<s;i++)
+    {
+        printf("\na[%d] = ",i);
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<s;i++)
+    {
+        for(j=0;j<s-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<s;i++)
+    {
+        printf("%d\t",a[i]);
+}
+}
+
 
